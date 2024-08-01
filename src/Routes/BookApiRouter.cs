a10a4f655348e47fd167e7b4c81367752741e98b
Marcos Hidalgo Nunes <marcoshidalgonunes@msn.com>
2024-08-01 11:47:00 -0300

    Added response status codes

--- src/Routes/BookApiRouter.cs
+++ src/Routes/BookApiRouter.cs
@@ -9,51 +9,76 @@ internal static class BookApiRouter
 
     internal static RouteGroupBuilder AddBookApi(this RouteGroupBuilder group)
     {
-        group.MapGet(apiName, async (IBookRepository db) =>
-        {
-            return await db.ReadAllAsync();
-        });
-
-        group.MapGet($"{apiName}/{{id}}", async (string? id, IBookRepository db) =>
-        {
-            return await db.ReadByIdAsync(id) is Book book
-                ? Results.Ok(book)
-                : Results.NotFound();
-        });
-
-        group.MapGet($"{apiName}/{{criteria}}/{{search}}", async (string criteria, string search, IBookRepository db) =>
-        {
-            return await db.ReadByCriteriaAsync(criteria, search);
-        });
-
-        group.MapPost(apiName, async (Book book, IBookRepository db) =>
-        {
-            await db.CreateAsync(await book.ValidateAsync());
-
-            return Results.Created($"/{book.id}", book);
-        });
-
-        group.MapPut(apiName, async (Book bookIn, IBookRepository db) =>
-        {
-            if (await db.ReadByIdAsync(bookIn.id) is Book book)
+        group
+            .MapGet(apiName, async (IBookRepository db) =>
             {
-                await db.UpdateAsync(await bookIn.ValidateAsync());
-                return Results.NoContent();
-            }
+                return await db.ReadAllAsync();
+            })
+            .Produces<List<Book>>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status500InternalServerError);
 
-            return Results.NotFound();
-        });
-
-        group.MapDelete($"{apiName}/{{id}}", async (string? id, IBookRepository db) =>
-        {
-            if (await db.ReadByIdAsync(id) is Book book)
+        group
+            .MapGet($"{apiName}/{{id}}", async (string? id, IBookRepository db) =>
             {
-                await db.DeleteAsync(id);
-                return Results.NoContent();
-            }
+                return await db.ReadByIdAsync(id) is Book book
+                    ? Results.Ok(book)
+                    : Results.NotFound();
+            })
+            .Produces(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status500InternalServerError);
 
-            return Results.NotFound();
-        });
+        group
+            .MapGet($"{apiName}/{{criteria}}/{{search}}", async (string criteria, string search, IBookRepository db) =>
+            {
+                return await db.ReadByCriteriaAsync(criteria, search);
+            })
+            .Produces<List<Book>>(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status500InternalServerError);
+
+        group
+            .MapPost(apiName, async (Book book, IBookRepository db) =>
+            {
+                await db.CreateAsync(await book.ValidateAsync());
+
+                return Results.Created($"/{book.id}", book);
+            })
+            .Produces(StatusCodes.Status201Created)
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status422UnprocessableEntity)
+            .Produces(StatusCodes.Status500InternalServerError);
+
+        group
+            .MapPut(apiName, async (Book bookIn, IBookRepository db) =>
+            {
+                if (await db.ReadByIdAsync(bookIn.id) is Book book)
+                {
+                    await db.UpdateAsync(await bookIn.ValidateAsync());
+                    return Results.NoContent();
+                }
+
+                return Results.NotFound();
+            })
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status422UnprocessableEntity)
+            .Produces(StatusCodes.Status500InternalServerError);
+
+        group
+            .MapDelete($"{apiName}/{{id}}", async (string? id, IBookRepository db) =>
+            {
+                if (await db.ReadByIdAsync(id) is Book book)
+                {
+                    await db.DeleteAsync(id);
+                    return Results.NoContent();
+                }
+
+                return Results.NotFound();
+            })
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound)
+            .Produces(StatusCodes.Status500InternalServerError);
 
         return group;
     }
